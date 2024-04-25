1f82d08e1aff38251d256c84861d8572ef7b6e9d
Jeevan Gupta <jeevan.ram.gupta@gmail.com>
2024-04-25 18:04:49 +0530

    code to extract insight from blog post pdf

--- README.md
+++ README.md
@@ -1,14 +1,26 @@
 
 PDF Insight Extractor 
     
-    - using Anthropic Claude AI and streamlit a UI library in Python
+    - using Anthropic Claude AI API/ Open AI GPT API and streamlit a UI library in Python
+
+Getting Started Steps: 
+    1. create vertuial environment using below command
+        python3 -m venv env
+
+    2. activate vertuial environment
+        source env/bin/activate
+
+    3. create .env system file to store API key
+
+    4. use below command to include above .env file in system
+        source .env
 
 
-1. Resume Insight Extractor
+1. Resume Insight Extractor - extract usefull data from resume
     to run applitcation use below command.
         streamlit run resume_insight_extractor.py
 
 
-    NOTE: before this store your claude api key in .env file ANTHROPIC_API_KEY="<your API key>"
-    and run below command
-    source .env
+1. Blog Post Insight Extractor - summarise blog post, extract title, keywords, sentiment, summary, and more
+    to run applitcation use below command.
+        streamlit run blog_post_insight_extractor.py
---
+++ blog_post_insight_extractor.py
@@ -0,0 +1,53 @@
+import os
+import streamlit as st
+from PyPDF2 import PdfReader
+import openai
+from dotenv import load_dotenv
+load_dotenv()
+
+st.set_page_config("Blog Post (PDF) Insights Extractor", page_icon=":memo:")
+
+st.title("Blog Post PDF Insights Extractor")
+
+key = os.environ.get("OPENAI_API_KEY")
+#print(f"\n **** Key: {key}")
+
+openai.key = key
+uploaded_file = st.file_uploader("Select a Resume PDF", type="pdf")
+
+if uploaded_file is not None:
+    pdf_reader = PdfReader(uploaded_file)
+    text = ""
+
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+
+    st.subheader("Extracted PDF Contents")
+    st.text_area("", value=text, height=300)
+
+
+    with open("./blog_post_system_prompt.txt", "r") as f:
+        system_prompt = f.read()
+
+    main_prompt = "Here is an blog post: <blog_post>{}</blog_post>"
+
+    if st.button("Extract Insights"):
+        # st.subheader("Key")
+        # st.text_area("", value=key, height=100)
+
+        response = openai.chat.completions.create(
+            model="gpt-4-turbo-preview",
+            max_tokens=1024,
+            temperature=0.5,
+            seed=456,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role":"user", "content": main_prompt.format(text)}
+            ]
+        )
+
+        st.subheader("Extracted Insights")
+        if response.choices:
+            st.write(response.choices[0].message.content)
+
+
